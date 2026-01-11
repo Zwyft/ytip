df12f4430f229fc1dcfd3d9283ec0a4f9612110a
Zwyft <officialzwyft@gmail.com>
2026-01-11 08:24:17 -0600

    Fix Submodules: Define jbroot macro in YTLite to resolve implicit declaration

--- Tweaks/YTLite/Utils/NSBundle+YTLite.m
+++ Tweaks/YTLite/Utils/NSBundle+YTLite.m
@@ -1,19 +1,25 @@
 #import "NSBundle+YTLite.h"
 
+#import "NSBundle+YTLite.h"
+
+#define jbroot(path) path
+
 @implementation NSBundle (YTLite)
 
 + (NSBundle *)ytl_defaultBundle {
-    static NSBundle *bundle = nil;
-    static dispatch_once_t onceToken;
+  static NSBundle *bundle = nil;
+  static dispatch_once_t onceToken;
 
-    dispatch_once(&onceToken, ^{
-        NSString *tweakBundlePath = [[NSBundle mainBundle] pathForResource:@"YTLite" ofType:@"bundle"];
-        NSString *kBundlePath = jbroot(@"/Library/Application Support/YTLite.bundle");
+  dispatch_once(&onceToken, ^{
+    NSString *tweakBundlePath =
+        [[NSBundle mainBundle] pathForResource:@"YTLite" ofType:@"bundle"];
+    NSString *kBundlePath =
+        jbroot(@"/Library/Application Support/YTLite.bundle");
 
-        bundle = [NSBundle bundleWithPath:tweakBundlePath ?: kBundlePath];
-    });
+    bundle = [NSBundle bundleWithPath:tweakBundlePath ?: kBundlePath];
+  });
 
-    return bundle;
+  return bundle;
 }
 
 @end
